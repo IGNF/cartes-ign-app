23b0c06567850899096d5e90cfb5a6720196cbfa
azarz <amaury.zarzelli@gmail.com>
2023-07-27 14:57:29 +0200

    agnostic getfeatureinfo

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/js/index.js',
   output: {
     path: path.resolve(__dirname, 'www/js'),
     filename: 'index.bundle.js',
   },
-  devtool: 'inline-source-map',
 };
