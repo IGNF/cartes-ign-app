17d91ab0ce79b65cd6f9d3d8c0074c06b398869a
amaury.zarzelli <amaury.zarzelli@ign.fr>
2023-08-18 11:46:10 +0200

    conversion Leaflet->mapLibre : remove route and elevation from map-controls.js

--- src/js/map-controls.js
+++ src/js/map-controls.js
@@ -1,9 +1,6 @@
 import Globals from './globals';
-import DOM from './dom';
 import * as LayerSwitch from './layer-switch';
 import * as MenuDisplay from './menu-display';
-
-import { Geolocation } from '@capacitor/geolocation';
 
 let sideBySide = L.control.sideBySide();
 const map = Globals.map;
@@ -11,140 +8,12 @@ const map = Globals.map;
 let prevDataLayerDisplayed = '';
 
 function addMapControls() {
-
   // Échelle graphique
   L.control.scale({
     imperial: false,
     maxWidth: 150,
     position: "topleft",
   }).addTo(map);
-
-  /* GP plugin*/
-  // Geoportail widget route
-  const route = L.geoportalControl.Route({
-    apiKey: "calcul",
-    routeOptions: {
-      onSuccess: routeSuccess,
-      geometryInInstructions: false,
-    },
-  });
-  map.addControl(route);
-
-  // Geoportail widget elevationpath
-  const elevationpath = L.geoportalControl.ElevationPath({
-    apiKey: "calcul",
-    elevationPathOptions: {
-      httpMethod: "POST",
-      api: "WPS",
-      sampling: 200,
-    }
-  });
-  map.addControl(elevationpath);
-
-  // Route success callback
-  function routeSuccess(routeResponse) {
-    computeRouteElevation(routeResponse);
-  }
-
-  // Elevation path on route result
-  function computeRouteElevation(routeResponse) {
-    const geom = [];
-    const routeGeom = routeResponse.routeGeometry.coordinates;
-    for (var i = 0; i < routeGeom.length; i++) {
-      geom.push({
-        lon : routeGeom[i][0],
-        lat : routeGeom[i][1]
-      });
-    }
-    elevationpath._distance = parseFloat(routeResponse.totalDistance);
-    elevationpath._geometry = geom;
-    elevationpath.options.elevationPathOptions.sampling = 0;
-    if (geom.length < 200) {
-      elevationpath.options.elevationPathOptions.sampling = 200;
-    }
-    elevationpath._pictoContainer.style.display = "none";
-    elevationpath._panelContainer.style.display = "block";
-    elevationpath._altiRequest();
-    elevationpath.options.elevationPathOptions.sampling = 200;
-    MenuDisplay.updateScrollAnchors();
-  };
-
-  // Add route from my position
-  const reference = document.querySelector('form[id^="GProuteForm"]').firstChild.firstChild.lastChild;
-  const routeFromMe = document.createElement("label");
-  routeFromMe.id = "routeFromMe";
-  routeFromMe.classList.add("GPlocationOriginPointerImgFromMyPosition");
-  routeFromMe.title = "Itinéraire depuis ma position";
-  reference.parentNode.insertBefore(routeFromMe, reference);
-
-  routeFromMe.addEventListener("click", () => {
-    Geolocation.checkPermissions().then((status) => {
-      if (status.location != 'denied') {
-        Geolocation.getCurrentPosition().then((position) => {
-          const latlng = {lat: position.coords.latitude, lng: position.coords.longitude};
-          route._currentPoints[0]._inputShowPointerContainer.checked = true;
-          route._currentPoints[0]._inputAutoCompleteContainer.className = "GPlocationOriginHidden";
-          route._currentPoints[0]._inputCoordinateContainer.className = "GPlocationOriginVisible";
-          route._currentPoints[0]._setLabel();
-          route._currentPoints[0]._clearResults();
-          route._currentPoints[0]._setMarker(latlng, null, false);
-          route._currentPoints[0]._setCoordinate(latlng);
-          document.querySelector('input[id^="GProuteSubmit"]').click();
-        });
-      }
-    });
-  });
-
-  // Move controls in DOM
-  DOM.$bottomMenu.appendChild(document.querySelector("div[id^=GProute-]"));
-  document.querySelector("div[id^=GProuteResultsPanel-]").appendChild(document.querySelector("div[id^=GPelevationPath-]"));
-
-  document.querySelector("div[id^=GProutePanelClose-]").addEventListener('click', MenuDisplay.closeRoute);
-
-  const gpAddStage = document.querySelector("div[id^=GPlocationStageAdd-]");
-  gpAddStage.addEventListener('click', () => {
-    setTimeout( () => {
-      MenuDisplay.updateScrollAnchors();
-      if (Globals.currentScrollIndex != 2) {
-        window.scrollBy({
-          top: gpAddStage.clientHeight,
-          left: 0,
-          behavior : "smooth"
-        });
-      }
-    }, 250);
-  });
-
-  document.querySelector("[id^=GPshowRouteExclusionsPicto-]").addEventListener('click', () => {
-    setTimeout( () => {
-      MenuDisplay.updateScrollAnchors();
-      if (Globals.currentScrollIndex != 2) {
-      window.scrollBy({
-        top: document.querySelector("[id^=GProuteExclusions-]").clientHeight,
-        left: 0,
-        behavior : "smooth"
-      });
-      }
-
-    }, 600);
-  });
-
-  const gpLocationImgs = document.querySelectorAll(".GPlocationOriginPointerImg");
-  gpLocationImgs.forEach(el => el.addEventListener("click", () => {
-    if (Globals.firstClickNeeded) {
-      DOM.$map.click();
-    }
-    Globals.firstClickNeeded = false;
-  }));
-
-  document.querySelector("[id^=GProuteResultsNew-]").addEventListener("click", () => {
-    Globals.firstClickNeeded = true;
-  });
-
-  document.querySelector("[id^=GPelevationPathProfil]").addEventListener("touchmove", (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-  });
 }
 
 function addSideBySide() {
