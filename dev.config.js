5d4e6ed3eb428d82aeff171b5caa53ea222aafe0
amaury.zarzelli <amaury.zarzelli@ign.fr>
2023-08-22 10:46:07 +0200

    move css to webpack src, cut index.css in pieces

--- dev.config.js
+++ dev.config.js
@@ -1,10 +1,33 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: 'development',
   entry: './src/js/index.js',
   output: {
-    path: path.resolve(__dirname, 'www/js'),
-    filename: 'index.bundle.js',
+    path: path.resolve(__dirname, 'www'),
+    filename: 'js/index.bundle.js',
+    assetModuleFilename: 'css/assets/[hash][ext][query]'
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/index.bundle.css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(ttf|png|svg)$/,
+        type: 'asset/resource',
+      },
+   ]
+  }
 };
