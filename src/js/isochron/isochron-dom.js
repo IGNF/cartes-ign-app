8837328e6cd92b689ea6c615e5cf16ccea931c61
jean-philippe.bazonnais <jean-philippe.bazonnais@ign.fr>
2023-09-14 17:48:47 +0200

    Mise en place de la maquette provisoire + test sur le DOM Shadow

--- src/js/isochron/isochron-dom.js
+++ src/js/isochron/isochron-dom.js
@@ -1,8 +1,17 @@
+import MyCSS from "!!raw-loader!../../css/isochron.css";
+
 /**
  * DOM du contrôle du calcul d'isochrone
  * @mixin IsochronDOM
  */
 let IsochronDOM = {
+
+    /** 
+     * DOM utile pour la classe métier
+     */
+    dom : {
+        container : null
+    },
 
     /**
      * transforme un texte html en dom
@@ -27,7 +36,7 @@ let IsochronDOM = {
         if (support()) {
             var parser = new DOMParser();
             var doc = parser.parseFromString(str, 'text/html');
-            return doc.body;
+            return doc.body.firstChild;
         }
 
         // Otherwise, fallback to old-school method
@@ -43,18 +52,80 @@ let IsochronDOM = {
      * @public
      */
     getContainer () {
-        var container = `
-        <div id="" class="">
-            <!-- titre -->
-            <!-- location -->
-            <!-- type de calcul : distance / temps -->
-            <!-- transport -->
-            <!-- filtres POI -->
-            <!-- option d'affichage -->
-            <!-- bouton de calcul -->
+        // contexte
+        var self = this;
+
+        // container avec la CSS inline
+        var strContainer = `
+        <style>${MyCSS}</style>
+        <div id="isochronContainer">
+            <form id="isochronForm" onkeypress="return event.keyCode != 13;">
+                <!-- titre -->
+                <p class="">Lancer une recherche à proximité</p> 
+                <!-- location -->
+                <input id="isochronLocation" class="inputIsochronLocation" type="text" placeholder="Saisir une adresse..." name="location" data-coordinates="">
+                <!-- type de calcul : distance / temps -->
+                <div class="divIsochronMode">
+                    <!-- TODO -->
+                </div>
+                <!-- transport -->
+                <div class="divIsochronTransport">
+                    <input id="isochronTransportPieton" type="radio" name="Transport" value="Pieton">
+                    <label class="lblIsochronTransport" for="isochronTransportPieton" title="A pied">A pied</label>
+                    <input id="isochronTransportVoiture" type="radio" name="Transport" value="Voiture">
+                    <label class="lblIsochronTransport" for="isochronTransportVoiture" title="Véhicule">Véhicule</label>
+                    <span class="glider"></span>
+                </div>
+                <!-- TODO filtres POI -->
+                <!-- TODO option d'affichage -->
+                <!-- bouton de calcul -->
+                <input id="isochronCompute" class="btnIsochronCompute" type="submit" value="Calculer">
+            </form>
         </div>
         `;
-        return this.stringToHTML(container).firstChild;
+
+        // transformation du container : String -> DOM
+        var container = this.stringToHTML(strContainer.trim());
+
+        // ajout du shadow DOM
+        const shadow = container.attachShadow({ mode: "open" });
+        shadow.innerHTML = strContainer.trim();
+
+        // FIXME 
+        // comment ajouter les CSS sur le shadow DOM ?
+        //  var css = new CSSStyleSheet();
+        //  css.replaceSync(MyCSS);
+        //  shadow.adoptedStyleSheets.push(css);
+
+        // ajout des listeners principaux :
+        // - le calcul
+        // - l'ouverture du menu de recherche
+
+        shadow.getElementById("isochronForm").addEventListener("submit", (e)  => {
+            e.preventDefault();
+            // TODO
+            // recuperer les valeurs des composants HTML:
+            // - transport
+            // - type
+            // - location
+
+            // passer les valeurs au service
+            self.compute({
+                transport : null,
+                type : null,
+                location : null
+            });
+
+            return false;
+        });
+
+        shadow.getElementById("isochronLocation").addEventListener("click", (e) => {
+            console.log(e);
+            // ouverture du menu de recherche
+            self.onOpenSearchLocation(e);
+        });
+
+        return container;
     },
 };
 
