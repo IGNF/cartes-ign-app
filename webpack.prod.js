d55287fb3bfaac9d231dbe046f7b32acb96090ea
azarz <amaury.zarzelli@ign.fr>
2023-12-07 15:40:55 +0100

    fix(ios): ios 13 compatibility and css issues

--- webpack.prod.js
+++ webpack.prod.js
@@ -24,6 +24,19 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(?:js|mjs|cjs)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: { 
+                "ios": "13"
+              }}]
+            ]
+          }
+        }
+      },
+      {
         test: /\.(scss|css)$/,
         use: [
           MiniCssExtractPlugin.loader,
