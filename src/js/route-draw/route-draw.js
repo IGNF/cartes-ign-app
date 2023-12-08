2dc1fe10217aeadf9e0111dd1541e8a9c75c209c
azarz <amaury.zarzelli@gmail.com>
2023-12-08 14:05:20 +0100

    feat(route-draw): drag waypoints

--- src/js/route-draw/route-draw.js
+++ src/js/route-draw/route-draw.js
@@ -74,7 +74,18 @@ class RouteDraw {
         this.elevation = new ElevationLineControl({target: document.getElementById("routedraw-elevationline")});
 
         // fonction d'event avec bind
-        this.boundOnAddWayPoint = this.#onAddWayPoint.bind(this);
+        this.handleAddWayPoint = this.#onAddWayPoint.bind(this);
+        this.handleTouchStart = this.#onTouchStart.bind(this);
+        this.handleTouchMove = this.#onTouchMove.bind(this);
+        this.handleTouchEnd = this.#onTouchEnd.bind(this);
+
+        // point actuellement déplacé par l'interactivité
+        this.movedPoint = null;
+        this.movedPointIndex = null;
+
+        // compteurs pour identifiants uniques de points et steps
+        this.nextPointId = 0;
+        this.nextStepId = 0;
 
         // annulation de la reqête fetch
         this.controller = new AbortController();
@@ -128,12 +139,74 @@ class RouteDraw {
      * ajout d'ecouteurs pour la saisie interactive
      */
     #listeners() {
-        this.map.on("click", this.boundOnAddWayPoint);
+        this.map.once("click", this.handleAddWayPoint);
+        this.map.on("touchstart", "route-draw-point", this.handleTouchStart);
 
-        // this.obj.on("addwaypoint", (e) => { this.#onAddWayPoint(e); });
         // this.obj.on("cancellast", (e) => { this.#onCancelLast(e); });
         // this.obj.on("restorenext", (e) => { this.#onRestoreNext(e); });
         // this.obj.on("deletepoint", (e) => { this.#onDeletePoint(e); });
+    }
+
+    /**
+     * ecouteur lors du déplacement d'un waypoint sur la carte - début
+     * @param {*} e
+     * @returns
+     */
+    #onTouchStart(e) {
+        e.preventDefault();
+        const feature = this.map.queryRenderedFeatures(e.point, {
+            layers: ["route-draw-point"],
+        })[0];
+        this.movedPointIndex = this.data.points.findIndex((point) => {
+            return point.properties?.id === feature?.properties?.id;
+        });
+        console.log(this.movedPointIndex);
+        this.movedPoint = this.data.points[this.movedPointIndex];
+        this.movedPoint.properties.highlight = true;
+        this.#updateSources();
+
+        this.map.on('touchmove', this.handleTouchMove);
+        this.map.once('touchend', this.handleTouchEnd);
+    }
+
+    /**
+     * ecouteur lors du déplacement d'un waypoint sur la carte - pendant
+     * @param {*} e
+     * @returns
+     */
+    #onTouchMove(e) {
+        const coords = e.lngLat;
+        this.movedPoint.geometry.coordinates = [coords.lng, coords.lat];
+        this.#updateSources();
+    }
+
+    /**
+     * ecouteur lors du déplacement d'un waypoint sur la carte - fin
+     * @param {*} e
+     * @returns
+     */
+    async #onTouchEnd(e) {
+        this.map.off("click", this.handleAddWayPoint);
+        this.map.off("touchmove", this.handleTouchMove);
+        this.map.off("touchstart", "route-draw-point", this.handleTouchStart);
+        const index = this.movedPointIndex;
+        const address = await this.#computePointName(e.lngLat);
+        this.movedPoint.properties.highlight = false;
+        this.movedPoint.properties.name = address;
+        this.movedPoint = null;
+        this.movedPointIndex = null;
+        this.#updateSources();
+        var promises = [];
+        if (index > 0) {
+            promises.push(this.#computeStep(index - 1));
+        }
+        if (index < this.data.steps.length) {
+            promises.push(this.#computeStep(index));
+        }
+        Promise.all(promises).then( () => {
+            this.map.on("click", this.handleAddWayPoint);
+            this.map.on("touchstart", "route-draw-point", this.handleTouchStart);
+        });
     }
 
     /**
@@ -142,11 +215,16 @@ class RouteDraw {
      * @returns
      */
     async #onAddWayPoint(e) {
-        // On empêche l'intéraction tant que les opérations ne sont pas terminées
+        // On empêche l'intéraction tant que les opérations ne sont pas terminées (map.once)
+        // Si on a cliqué sur un waypoint, on ne fait rien)
+        if(this.map.getSource(this.configuration.pointsource) && this.map.queryRenderedFeatures(e.point, {
+            layers: ["route-draw-point"],
+        })[0]) {
+            this.map.once("click", this.handleAddWayPoint);
+            return;
+        }
         // TODO: Patience
-        this.map.off("click", this.boundOnAddWayPoint);
         var coordinates = e.lngLat;
-        var bResponse = false;
         var length = this.data.points.push({
             type: "Feature",
             geometry: {
@@ -154,9 +232,48 @@ class RouteDraw {
                 coordinates: [coordinates.lng, coordinates.lat]
             },
             properties: {
-                name: ""
+                name: "",
+                id: this.nextPointId,
             }
-        })
+        });
+        this.nextPointId++;
+
+        const address = await this.#computePointName(coordinates);
+
+        // on ajoute les resultats dans le tableau (avec la length récupérée avant la promesse pour gérer l'asynchrone)
+        this.data.points[length - 1].geometry.coordinates = [coordinates.lng, coordinates.lat];
+        this.data.points[length - 1].properties.name = address;
+
+        // Si pas encore de source, on ajoute les sources et layers à la carte
+        if (this.map.getSource(this.configuration.pointsource)) {
+            this.#updateSources();
+        } else {
+            this.#addSourcesAndLayers();
+        }
+
+        // Pas d'autre étape s'il n'y a qu'un point (premier point)
+        if (this.data.points.length < 2) {
+            if (Globals.backButtonState === "routeDraw") {
+                this.map.once("click", this.handleAddWayPoint);
+            }
+            return
+        }
+
+        await this.#computeStep(this.data.points.length - 2);
+
+        // Affichage et réactivation de l'intéractivité si on est toujours dans le tracé d'itinéraire
+        if (Globals.backButtonState === "routeDraw") {
+            this.map.once("click", this.handleAddWayPoint);
+        }
+
+    }
+
+    /**
+     *  calcule le nom d'un point via reverse geocoding
+     * @param {MapLibreGL.LngLat}
+     */
+    async #computePointName(coordinates){
+        let bResponse = false;
         try {
             await Reverse.compute({
                 lon : coordinates.lng,
@@ -166,157 +283,119 @@ class RouteDraw {
         } catch(err) {
             bResponse = false;
         } finally {
-            var a = (bResponse) ? Reverse.getAddress() : coordinates.lng.toFixed(6) + ", " + coordinates.lat.toFixed(6);
-            var address = a;
+            const a = (bResponse) ? Reverse.getAddress() : coordinates.lng.toFixed(6) + ", " + coordinates.lat.toFixed(6);
+            let address = a;
             if (bResponse) {
+                address = "";
                 if (a.street) {
                     address = a.street + ", "
                 }
-                address += a.city + ", " + a.postcode;
-            }
-            // on ajoute les resultats dans le tableau (avec la length récupérée avant la promesse pour gérer l'asynchrone)
-            this.data.points[length - 1].geometry.coordinates = [coordinates.lng, coordinates.lat];
-            this.data.points[length - 1].properties.name = address;
-
-            var pointsource = this.map.getSource(this.configuration.pointsource);
-            if (pointsource) {
-                pointsource.setData({
-                    type: "FeatureCollection",
-                    features: this.data.points,
-                });
-            } else {
-                this.#addSourcesAndLayers();
-            }
-
-            // Pas d'autre étape s'il n'y a qu'un point (premier point)
-            if (this.data.points.length < 2) {
-                if (Globals.backButtonState === "routeDraw") {
-                    this.map.on("click", this.boundOnAddWayPoint);
-                }
-                return
-            }
-
-            // saisie libre
-            var previousPoint = this.data.points[length - 2];
-            if (this.mode === 0) {
-                var distance = new MapLibreGL.LngLat(coordinates.lng, coordinates.lat).distanceTo(new MapLibreGL.LngLat(...previousPoint.geometry.coordinates));
-                var duration = distance / (4 / 3.6); // 4 km/h divisé par 3.6 pour m/s
-                if (this.data.steps.length > 0) {
-                    var allCoordinates = this.data.steps.map((step) => step.geometry.coordinates).flat();
-                    allCoordinates.push([coordinates.lng, coordinates.lat]);
-                } else {
-                    allCoordinates = [previousPoint.geometry.coordinates, [coordinates.lng, coordinates.lat]];
-                }
-                var stepCoords = [
-                    previousPoint.geometry.coordinates,
-                    [coordinates.lng, coordinates.lat]
-                ];
-            // saisie guidée
-            } else if (this.mode === 1) {
-                var url = this.configuration.api +
-                this.configuration.template({
-                    start: {lng: previousPoint.geometry.coordinates[0], lat: previousPoint.geometry.coordinates[1]},
-                    end: coordinates,
-                    profile: this.transport,
-                });
-                this.loading = true;
-                try {
-                    var response = await fetch(url, { signal: this.controller.signal });
-                    var json = await response.json();
-                } catch (err) {
-                    this.loading = false;
-                    // TODO "Erreur lors de l'ajout du point"
-                    this.map.on("click", this.boundOnAddWayPoint);
-                    return;
-                }
-                this.loading = false;
-                // TODO REMOVE ME IMPORTANT à supprimer après passage en POST GPF
-                if (json.geometry.coordinates.length > 110) {
-                    var gcd = function(a, b) {
-                        if (b < 0.0000001) return a;
-                        return gcd(b, Math.floor(a % b));
-                    };
-
-                    let proportionToRemove = ((json.geometry.coordinates.length - 110) / json.geometry.coordinates.length).toFixed(2);
-                    var len = proportionToRemove.toString().length - 2;
-                    var denominator = Math.pow(10, len);
-                    var numerator = proportionToRemove * denominator;
-                    var divisor = gcd(numerator, denominator);
-                    numerator /= divisor;
-                    denominator /= divisor;
-                    let newrouteCoords = []
-                    for (let i=0; i<json.geometry.coordinates.length; i++) {
-                        let demPort = i%denominator;
-                        if (demPort >= numerator) {
-                            newrouteCoords.push(json.geometry.coordinates[i])
-                        }
-                    }
-                    json.geometry.coordinates = newrouteCoords;
-                }
-                // END REMOVEME
-                if (this.data.steps.length > 0) {
-                    var allCoordinates = this.data.steps.map((step) => step.geometry.coordinates).flat();
-                    allCoordinates.push(...json.geometry.coordinates);
-                } else {
-                    allCoordinates = json.geometry.coordinates;
-                }
-                this.data.points[length - 1].geometry.coordinates = allCoordinates.slice(-1)[0];
-                this.data.points[length - 2].geometry.coordinates = allCoordinates[0];
-                var stepCoords = json.geometry.coordinates;
-                var distance = json.distance;
-                var duration = json.duration;
-
-                var pointsource = this.map.getSource(this.configuration.pointsource);
-                pointsource.setData({
-                    type: "FeatureCollection",
-                    features: this.data.points,
-                });
-            }
-
-            var step = {
-                type: "Feature",
-                geometry: {
-                    type: "LineString",
-                    coordinates: stepCoords
-                },
-                properties: {
-                    start_name: previousPoint.properties.name,
-                    end_name: address,
-                    duration: duration,
-                    distance: distance,
-                    dplus: 0,
-                    dminus: 0,
+                address += a.city;
+                if (a.postcode) {
+                    address += ", " + a.postcode;
                 }
             }
-            this.data.steps.push(step);
-            var linesource = this.map.getSource(this.configuration.linesource);
-            linesource.setData({
-                type: "FeatureCollection",
-                features: this.data.steps,
+            return address;
+        }
+    }
+
+    /**
+     *  (re)calcule une étape du tracé a un index donné
+     * @param {int} index
+     */
+    async #computeStep(index) {
+        const firstPoint = this.data.points[index];
+        const lastPoint = this.data.points[index + 1];
+        // saisie libre
+        if (this.mode === 0) {
+            var distance = new MapLibreGL.LngLat(
+                lastPoint.geometry.coordinates[0], lastPoint.geometry.coordinates[1]
+            ).distanceTo(
+                new MapLibreGL.LngLat(firstPoint.geometry.coordinates[0], firstPoint.geometry.coordinates[1])
+            );
+            var duration = distance / (4 / 3.6); // 4 km/h divisé par 3.6 pour m/s
+            var stepCoords = [
+                firstPoint.geometry.coordinates,
+                lastPoint.geometry.coordinates
+            ];
+        // saisie guidée
+        } else if (this.mode === 1) {
+            var url = this.configuration.api +
+            this.configuration.template({
+                start: {lng: firstPoint.geometry.coordinates[0], lat: firstPoint.geometry.coordinates[1]},
+                end: {lng: lastPoint.geometry.coordinates[0], lat: lastPoint.geometry.coordinates[1]},
+                profile: this.transport,
             });
-
-            this.elevation.setCoordinates(allCoordinates);
-            var lastDPlus = this.elevation.dplus;
-            var lastDMinus = this.elevation.dminus;
+            this.loading = true;
             try {
-                await this.elevation.compute();
-            } catch(err) {
+                var response = await fetch(url, { signal: this.controller.signal });
+                var json = await response.json();
+            } catch (err) {
+                this.loading = false;
                 // TODO "Erreur lors de l'ajout du point"
-                this.map.on("click", this.boundOnAddWayPoint);
+                this.map.once("click", this.handleAddWayPoint);
                 return;
             }
-            this.data.steps.slice(-1)[0].properties.dplus = this.elevation.dplus - lastDPlus;
-            this.data.steps.slice(-1)[0].properties.dminus = this.elevation.dminus - lastDMinus;
-            this.data.distance += distance;
-            this.data.duration += distance / (4 / 3.6);
-            this.data.dplus = this.elevation.dplus;
-            this.data.dminus = this.elevation.dminus;
-            this.__updateRouteInfo(this.data);
+            this.loading = false;
+            lastPoint.geometry.coordinates = json.geometry.coordinates.slice(-1)[0];
+            firstPoint.geometry.coordinates = json.geometry.coordinates[0];
+            var stepCoords = json.geometry.coordinates;
+            var distance = json.distance;
+            var duration = json.duration;
 
-            // Affichage et réactivation de l'intéractivité si on est toujours dans le tracé d'itinéraire
-            if (Globals.backButtonState === "routeDraw") {
-                this.map.on("click", this.boundOnAddWayPoint);
+            this.#updateSources();
+        }
+
+        var step = {
+            type: "Feature",
+            geometry: {
+                type: "LineString",
+                coordinates: stepCoords
+            },
+            properties: {
+                start_name: firstPoint.properties.name,
+                end_name: lastPoint.properties.name,
+                duration: duration,
+                distance: distance,
+                dplus: 0,
+                dminus: 0,
+                id: this.nextStepId,
             }
+        }
+        this.nextStepId++;
+
+        // Correction du dplus total si on modifie un step déjà enregistré
+        let dpluscorrection = 0;
+        let dminuscorrection = 0;
+        if (index >= this.data.steps.length) {
+            this.data.steps.push(step);
+        } else {
+            dpluscorrection = this.data.steps[index].properties.dplus;
+            dminuscorrection = this.data.steps[index].properties.dminus;
+            this.data.steps[index] = step;
+        }
+        this.#updateSources();
+
+        var allCoordinates = this.data.steps.map((step) => step.geometry.coordinates).flat();
+
+        this.elevation.setCoordinates(allCoordinates);
+        var lastDPlus = this.elevation.dplus - dpluscorrection;
+        var lastDMinus = this.elevation.dminus - dminuscorrection;
+        try {
+            await this.elevation.compute();
+        } catch(err) {
+            // TODO "Erreur lors de l'ajout du point"
+            this.map.once("click", this.handleAddWayPoint);
+            return;
+        }
+        this.data.steps[index].properties.dplus = this.elevation.dplus - lastDPlus;
+        this.data.steps[index].properties.dminus = this.elevation.dminus - lastDMinus;
+        this.data.distance += distance;
+        this.data.duration += distance / (4 / 3.6);
+        this.data.dplus = this.elevation.dplus;
+        this.data.dminus = this.elevation.dminus;
+        if (Globals.backButtonState === "routeDraw") {
+            this.__updateRouteInfo(this.data);
         }
     }
 
@@ -352,6 +431,24 @@ class RouteDraw {
     }
 
     /**
+     * met à jour les sources de données pour l'affichage
+     */
+    #updateSources() {
+        var linesource = this.map.getSource(this.configuration.linesource);
+        linesource.setData({
+            type: "FeatureCollection",
+            features: this.data.steps,
+        });
+
+        var pointsource = this.map.getSource(this.configuration.pointsource);
+        pointsource.setData({
+            type: "FeatureCollection",
+            features: this.data.points,
+        });
+
+    }
+
+    /**
      * togle entre saisie libre et guidée
      * @public
      */
@@ -370,7 +467,6 @@ class RouteDraw {
      * @public
      */
     clear () {
-        this.map.off("click", this.boundOnAddWayPoint);
         this.elevation.clear();
         if (this.loading) {
             this.controller.abort();
