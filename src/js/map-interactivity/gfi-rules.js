33c4e8b55146c4900629d93b2a3b80b67b3cd5a3
amaury.zarzelli <amaury.zarzelli@ign.fr>
2024-03-05 17:52:36 +0100

    feat(map-interactivity): use json GFI, example layer rule

---
+++ src/js/map-interactivity/gfi-rules.js
@@ -0,0 +1,38 @@
+const gfiRules = {
+    "TRANSPORTS.DRONES.RESTRICTIONS$GEOPORTAIL:OGC:WMTS": {
+      "properties": ["limite"],
+      "title": "@limite",
+      "body": [["Limite", "@limite"], ["Source des données", "DGAC"]]
+    },
+
+    /**
+     * Parse le GFI
+     * @param {*} rule règle de parsing de GFI issue de ce fichier
+     * @param {*} gfi gfi en mode json
+     * @returns {Object} {title: ..., html: ...} pour l'affichage
+     */
+    parseGFI: (rule, gfi) => {
+      const result = {};
+      const featureProperties = gfi.features[0].properties;
+      if (rule["title"][0] === "@") {
+        result.title = featureProperties[rule.title.split("@")[1]];
+      } else {
+        result.title = rule.title;
+      }
+      let body = "<div>"
+      rule["body"].forEach( (bodyElement) => {
+        let value;
+        if (bodyElement[1][0] === "@") {
+          value = featureProperties[bodyElement[1].split("@")[1]];
+        } else {
+          value = bodyElement[1];
+        }
+        body += `<p>${bodyElement[0]} : ${value}</p>`
+      });
+      body += "</div>";
+      result.html = body;
+      return result
+    }
+};
+
+export default gfiRules;
