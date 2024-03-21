990653d22904ea9af11bf21d1b913817b2a03518
amaury.zarzelli <amaury.zarzelli@ign.fr>
2024-03-21 14:02:47 +0100

    fix(plan-ign): plan ign style

--- utils/convertOLZoomsToMapblireZooms.py
+++ utils/convertOLZoomsToMapblireZooms.py
@@ -2,10 +2,10 @@ import json
 
 def converZoomsLayers(style):
   for layer in style["layers"]:
-    if "minzoom" in layer and layer["source"] == "plan_ign" and layer["minzoom"] != 0:
-      layer["minzoom"] += 1
+    # if "minzoom" in layer and layer["source"] == "plan_ign" and layer["minzoom"] != 0:
+      # layer["minzoom"] -= 1
     if "maxzoom" in layer:
-      layer["maxzoom"] += 2
+      layer["maxzoom"] += 1
 
     if "paint" not in layer:
       continue
@@ -16,7 +16,7 @@ def converZoomsLayers(style):
       if isinstance(layer["paint"][prop], list) and layer["paint"][prop][0] == "step":
         for i in range(3, len(layer["paint"][prop])):
           if i % 2 == 1:
-            layer["paint"][prop][i] += 1
+            layer["paint"][prop][i] -= 1
 
 def convertFileZooms(path):
   with open(path, encoding="utf-8") as file:
