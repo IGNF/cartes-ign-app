60344424aef536f7be4b1d71e9ee1dd4d56f5c03
azarz <amaury.zarzelli@gmail.com>
2024-04-30 14:20:10 +0200

    fix(wms-gfi): click only works on tip of bubble

--- src/js/map-interactivity/map-interactivity.js
+++ src/js/map-interactivity/map-interactivity.js
@@ -316,7 +316,7 @@ class MapInteractivity {
       `&STYLE=${layer[1].style}`;
       if (layer[0].split(":").slice(-1)[0] === "WMS") {
         // https://wiki.openstreetmap.org/wiki/Zoom_levels
-        const resolution = 40075016.686 * Math.cos(layer[1].clickCoords.lat * Math.PI/180) / Math.pow(2, layer[1].computeZoom + 7);
+        const resolution = 40075016.686 * Math.cos(layer[1].clickCoords.lat * Math.PI/180) / Math.pow(2, layer[1].computeZoom + 8);
         const clickMercatorCoords = proj4(proj4.defs("EPSG:4326"), proj4.defs("EPSG:3857"), [layer[1].clickCoords.lng, layer[1].clickCoords.lat]);
         // https://gis.stackexchange.com/questions/79201/lat-long-values-in-a-wms-getfeatureinfo-request
         const bottomLeft = [clickMercatorCoords[0] - 50 * resolution, clickMercatorCoords[1] - 50 * resolution];
@@ -384,7 +384,7 @@ class MapInteractivity {
         `&STYLE=${layer[1].style}&INFOFORMAT=application/json&I=${layer[1].tiles.tilePixel.x}&J=${layer[1].tiles.tilePixel.y}`;
       if (layer[0].split(":").slice(-1)[0] === "WMS") {
         // https://wiki.openstreetmap.org/wiki/Zoom_levels
-        const resolution = 40075016.686 * Math.cos(layer[1].clickCoords.lat * Math.PI/180) / Math.pow(2, layer[1].computeZoom + 7);
+        const resolution = 40075016.686 * Math.cos(layer[1].clickCoords.lat * Math.PI/180) / Math.pow(2, layer[1].computeZoom + 8);
         const clickMercatorCoords = proj4(proj4.defs("EPSG:4326"), proj4.defs("EPSG:3857"), [layer[1].clickCoords.lng, layer[1].clickCoords.lat]);
         // https://gis.stackexchange.com/questions/79201/lat-long-values-in-a-wms-getfeatureinfo-request
         const bottomLeft = [clickMercatorCoords[0] - 50 * resolution, clickMercatorCoords[1] - 50 * resolution];
