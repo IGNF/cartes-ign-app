2d11f766bbbeb136b8ae19cebd0c2791f4e2c496
azarz <amaury.zarzelli@gmail.com>
2024-06-06 16:48:27 +0200

    feat(plan-ign): offset line-width zoom levels -2

--- utils/changeZoomLevels.py
+++ utils/changeZoomLevels.py
@@ -1,29 +1,30 @@
 import json
 
-def convertZoomsLayers(style):
-  for layer in style["layers"]:
-    # if "minzoom" in layer and layer["source"] == "plan_ign" and layer["minzoom"] != 0:
-      # layer["minzoom"] -= 1
-    if "maxzoom" in layer:
-      layer["maxzoom"] += 1
+PROPERTIES_TO_CHANGE = [
+  "line-width"
+]
 
+def changeZoomLevels(style, offset=-2):
+  for layer in style["layers"]:
     if "paint" not in layer:
       continue
     for prop in layer["paint"]:
+      if not (prop in PROPERTIES_TO_CHANGE):
+        continue
       if isinstance(layer["paint"][prop], dict) and "stops" in layer["paint"][prop]:
         for stop in layer["paint"][prop]["stops"]:
-          stop[0] += 1
+          stop[0] += offset
       if isinstance(layer["paint"][prop], list) and layer["paint"][prop][0] == "step":
         for i in range(3, len(layer["paint"][prop])):
           if i % 2 == 1:
-            layer["paint"][prop][i] -= 1
+            layer["paint"][prop][i] += offset
 
-def convertFileZooms(path):
+def changeFileZooms(path):
   with open(path, encoding="utf-8") as file:
     style = json.load(file)
-  convertZoomsLayers(style)
+  changeZoomLevels(style)
   with open(path, "w", encoding="utf-8", newline="") as file:
     json.dump(style, file, indent=2, ensure_ascii=False)
 
 if __name__ == "__main__":
-  convertFileZooms("www/data/plan-ign-interactif-style.json")
+  changeFileZooms("www/data/plan-ign-interactif-style.json")
