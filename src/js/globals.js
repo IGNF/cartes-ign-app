b0b4b218f9bed21fdb8a4401024e981d6a85b8bf
azarz <amaury.zarzelli@gmail.com>
2025-04-02 16:56:37 +0200

    feat: add track recording and route snap

--- src/js/globals.js
+++ src/js/globals.js
@@ -102,6 +102,9 @@ let compareLandmark = null;
 // Global control offline maps
 let offlineMaps = null;
 
+// Global control track record
+let trackRecord = null;
+
 // Global control immersive notifications
 let immersiveNotifications = null;
 
@@ -147,7 +150,7 @@ if (!localStorage.getItem("walkingSpeed")) {
 // Are new place notifications enabled?
 let newPlaceNotifEnabled;
 if (!localStorage.getItem("newPlaceNotifEnabled")) {
-  newPlaceNotifEnabled = 1;
+  newPlaceNotifEnabled = 0;
 } else {
   newPlaceNotifEnabled = parseFloat(localStorage.getItem("newPlaceNotifEnabled"));
 }
@@ -194,4 +197,5 @@ export default {
   offlineMaps,
   immersiveNotifications,
   newPlaceNotifEnabled,
+  trackRecord,
 };
